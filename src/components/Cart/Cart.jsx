3390f5c5c53e3df490414ca3f6fb554d3dcdc8f7
shojol021 <shojol021@gmail.com>
2023-04-07 18:14:13 +0600

    update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './cart.css'
 
 const Cart = ({ cart, handleDeleteCart }) => {
+    let message;
+    if(cart.length === 0){
+        message = 'Please add some products'
+    }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
-                {
-                    cart.map(ts => <p key={ts._id}
-                    > {ts.name} <button 
+            <h2 className={`bold ${cart.length>2? 'purple': 'blue' }`}>Order Summary: {cart.length}</h2>
+            {message}
+            {
+                cart.map(ts => <p
+                    key={ts._id}
+                > {ts.name} <button
                     onClick={() => handleDeleteCart(ts._id)} >X</button></p>)
-                }
-                
+            }
+
         </div>
     );
 };
